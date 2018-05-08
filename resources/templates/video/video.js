6b38fbbcb06a6a933b7e5ea4cecabec7607c6b54
Anand Kumar <anandkumar@triconinfotech.com>
2018-05-09 01:52:20 +0530

    removed obsolete code and modify interview.js

--- resources/templates/video/video.js
+++ resources/templates/video/video.js
@@ -3,89 +3,55 @@
  * @param app
  * @returns {Video}
  */
-function Video(app)
- {
+function Video(app) {
   // Private vars
   // ------------
-  function init()
-   {
-    if ($('#lightbox_toolkit_lb').length > 0)
-     {
+  function init() {
+    if ($('#lightbox_toolkit_lb').length > 0) {
       // Do nothing since video.js is loading into a lightbox
-     }
-    else
-     {
+    } else {
       var thisFlowplayerIndex = $('a.flowplayer').length - 1,
-      vidWidth = app.template.getData().content[0].videoWidth || '600';
-      
-      if(app.template.getData().content[0].videoURL.type === 'audio'){
-       $('body').addClass('audio_only');
+        vidWidth = app.template.getData().content[0].videoWidth || '600';
+
+      if (app.template.getData().content[0].videoURL.type === 'audio') {
+        $('body').addClass('audio_only');
       }
-      
+
       $('#data_videoURL').css('width', vidWidth + 'px');
-      
-      $('body').one('template.loaded', function()
-                                        {
-                                         app.template.fixImgPaths();
-                                        });
-     }
-   }
-  
-  
-  App.prototype.toolkitInit = function (args)
-                       {
-//                        app.template.flowplayerHelper().unload('#data_videoURL a.flowplayer');
-                        $('body').one('lightbox.close', function()
-                                                         {
-//                                                          var flowplayerHTML = $('#data_videoURL').html();
-//                                                          $('#data_videoURL').html(flowplayerHTML.replace(/(&nbsp;)*/g,""));
-//                                                         
-//                                                                         app.template.flowplayerHelper().registerFlowplayer({
-//                                                                          playerId : $f('*').length,
-//                                                                          DOMelement : 'a.flowplayer:eq(0)'
-//                                                                         });
-//                                                          
-//                                                          $('a.flowplayer:eq(0)').flowplayer({
-//                                                                                          src : app.baseURL + 'resources/js/lib/flowplayer/flowplayer-3.2.2.swf',
-//                                                                                          wmode : 'opaque'
-//                                                                                         }, 
-//                                                                                         {
-//                                                                                          clip : {
-//                                                                                                  'autoPlay' : false
-//                                                                                                 }
-//                                                                                         });
-//                                                          
-//                                                          // This is to signify when a flowplayer has been reloaded artificially, interview template relies on this and foreseeably simconvo if it is coupled with toolkit
-//                                                          $('#data_videoURL').addClass('reloaded');
-                                                          
-                                                          if ($('body.interview #data_videoURL').length > 0)
-                                                           {
-                                                            try
-                                                            {      
-                                                             flowplayer('*').each(function()
-                                                               {
-                                                                console.warn(this.getState())
-                                                                if (this.isLoaded() === true)
-                                                                 {
-                                                                  this.stop();
-                                                                  this.close();
-                                                                  this.unload();
-                                                                 }
-                                                               });
-                                                            }
-                                                           catch(e)
-                                                            {
-//                                                             alert(e)
-                                                            }
-                                                           setVideo();
-                                                            return;
-                                                           }
-                                                         });
-                       }
-  
+
+      $('body').one('template.loaded', function() {
+        app.template.fixImgPaths();
+      });
+    }
+  }
+
+
+  App.prototype.toolkitInit = function(args) {
+    // No trigger for below piece of code and hence its obsolete
+    // app.template.flowplayerHelper().unload('#data_videoURL a.flowplayer');
+    // $('body').one('lightbox.close', function() {
+    //   if ($('body.interview #data_videoURL').length > 0) {
+    //     try {
+    //       flowplayer('*').each(function() {
+    //         console.warn(this.getState())
+    //         if (this.isLoaded() === true) {
+    //           this.stop();
+    //           this.close();
+    //           this.unload();
+    //         }
+    //       });
+    //     } catch (e) {
+    //       //                                                             alert(e)
+    //     }
+    //     setVideo();
+    //     return;
+    //   }
+    // });
+  }
+
   // One-time setup
   // --------------
   init();
- }
- 
+}
+
 App.prototype.thisTemplate = new Video(ale);
